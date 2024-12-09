bc4d34f052d449622b799cc74155bc1b67e6c93e
DeepthiReddy74 <reddydeepthi68699@gmail.com>
2024-12-09 15:41:31 +0530

    Add files via upload

---
+++ transposematrix.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class transposematrix
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        int c=sc.nextInt();
+        int arr[][]=new int[r][c];
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("before transpose");
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+            }
+            System.out.println("aftre transpose");
+            for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                System.out.print(arr[j][i]+" ");
+            }
+            System.out.println();
+            }
+        }
+    }
