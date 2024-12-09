e820207548c0f4000e218ab27057c33dfbbac862
DeepthiReddy74 <reddydeepthi68699@gmail.com>
2024-12-09 15:11:23 +0530

    Add files via upload

---
+++ D2array.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class  D2array                                                       
+{
+    public static void main(String args[])
+    {
+        //initilization of 2D array
+        int a[][]={{1,2},{3,4}};
+        //accessing 1D array in 2Darray
+        System.out.println(Arrays.toString(a[0]));
+        System.out.println(Arrays.toString(a[1]));
+    }
+}
